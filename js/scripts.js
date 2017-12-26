50d7a363ce8dd2d980fa27b6e42ba6df86b9a47a
Michał Świtoń <michal.switon@op.pl>
2017-12-26 21:19:14 +0100

    new function in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var a = 3;
+var b = 7;
+var value = (a*a) - (2*a*b) + (b*b);
+console.log("value is:" +value);
+if ( value == 0 ) {
+    console.log("value = 0")
+}
+else if ( value > 0 ) {
+    console.log("value is bigger than 0")
+}
