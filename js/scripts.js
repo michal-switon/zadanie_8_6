40cd4c0ca8e024a1e7c4f9ca5b8ac9f8bfeaa4d1
Michał Świtoń <michal.switon@op.pl>
2017-12-27 13:28:42 +0100

    fixed scripts.js and package.json

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 var a = 3;
 var b = 7;
 var value = (a*a) - (2*a*b) + (b*b);
-console.log("value is:" +value);
+console.log("value is: " +value);
 if ( value == 0 ) {
-    console.log("value = 0")
+    console.log("value = 0");
 }
 else if ( value > 0 ) {
-    console.log("value is bigger than 0")
+    console.log("value is bigger than 0");
+}
+else if ( value < 0 ) {
+    console.log("value is smaller than 0");
 }
